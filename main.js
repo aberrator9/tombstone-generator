affbf3002eb6a819b4047860044b61361b8103f2
Justin Linton <hello.aberrator@gmail.com>
2023-06-15 22:30:39 -0400

    Deal with a/an disambiguation.

--- main.js
+++ main.js
@@ -5,18 +5,19 @@ const graveText = document.getElementById("grave-text");
 // const adverbs = ['too', 'very', 'extremely', 'vaguely', 'sort of'];
 const adjectives = ["silly", "dumb", "happy", "hungry", "fast", "flat",
     "slippery", "fat", "skinny", "damned", "dastardly", "seaworthy",
-    "rotund", "unreliable", "scared"];
+    "rotund", "unreliable", "scared", "eager", "lively", "skeevy"];
 const nouns = ["dog", "hat", "hombre", "temptress", "fool", "varmint",
     "cat", "dark figure", "snail", "porcupine"];
 const endings = ["did 'em in", "ended their life", "was their undoing",
     "was the end of them", "did 'em", "was the last they ever saw",
     "sent 'em 'eavenward", "did get them in the end"]
 
-class templateBuilder {
-
+function getAdjective() {
+    const adj = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    return vowels.includes(adj[0]) ? "n " + adj : " " + adj;
 }
 
-graveText.textContent =
-    "A " + adjectives[Math.floor(Math.random() * adjectives.length)] + " "
+graveText.textContent = "A" + getAdjective() + " "
     + nouns[Math.floor(Math.random() * nouns.length)] + " "
     + endings[Math.floor(Math.random() * endings.length)] + ".";
